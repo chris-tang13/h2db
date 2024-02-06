795981d1ee04130c304ab43112b20fd43438c11e
weixin_55017680 <weixin_55017680@devide>
2024-02-06 13:07:00 +0800

    Tue Feb  6 13:07:00 CST 2024 inscode

--- src/main/java/com/chris/mapper/UserMapper.java
+++
@@ -1,9 +0,0 @@
-package com.chris.mapper;
-
-import com.baomidou.mybatisplus.core.mapper.BaseMapper;
-import com.chris.entity.User;
-import org.apache.ibatis.annotations.Mapper;
-
-@Mapper
-public interface UserMapper extends BaseMapper<User> {
-}
